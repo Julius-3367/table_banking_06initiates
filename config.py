cc65904dca62efaf303c3c37ef7773f27c1406e0
Julius-3367 <korirjulius001@gmail.com>
2024-11-05 09:44:31 +0300

    update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
     MONGO_URI = "mongodb://localhost:27017/table_banking_06initiates"
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'korirjulius001@gmail.com'
-    MAIL_PASSWORD = 'eymr jlbq gesm skms'
+    MAIL_USERNAME = 'your email'
+    MAIL_PASSWORD = 'your password'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
 
